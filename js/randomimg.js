6fbcff45a67195a5d889ec030696b531c2122790
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-12 22:12:53 +0300

    munu work

--- js/randomimg.js
+++
@@ -1,2 +0,0 @@
-﻿document.getElementsByTagName('h2')
-
---
+++ upinfo.js
@@ -0,0 +1,4 @@
+﻿upnf.insertAdjacentHTML("afterend",`<div style="color: #b6ff00">Совершенно новый прорывной хоррор от молодой инди студии</div>
+
+    <h1 style=" color: #b6ff00; display: flex;justify-content: center; align-items: center; font-size: 40px"><b>RAFN 2</b></h1>`);
+
