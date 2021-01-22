8afef468334762f2638f61bf27172ad1ea876c65
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-22 15:26:42 +0300

    ЮЮЮ

--- upinfo.js
+++ upinfo.js
@@ -1,4 +1,19 @@
 ﻿upnf.insertAdjacentHTML("afterend",`<div style="color: #b6ff00">Совершенно новый прорывной хоррор от молодой инди студии</div>
 
-    <h1 style=" color: #b6ff00; display: flex;justify-content: center; align-items: center; font-size: 40px"><b>RAFN 2</b></h1>`);
+    <h1 style=" color: #b6ff00; display: flex;justify-content: center; align-items: center; font-size: 40px"><b id="gamename">RAFN 2</b></h1>`);
 
+
+var clics = 0
+var b = document.getElementById("gamename");
+b.onclick = function () {
+    clics += 1;
+    if (clics == 10) {
+        b.innerHTML = "Run Away From the Nigger 2"
+    }
+    
+}
+
+b.onmouseout = function () {
+    b.innerHTML = "RAFN 2"
+    clics = 0
+}
