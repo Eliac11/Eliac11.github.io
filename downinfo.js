7c771f77fd44be7572378918ae2d812d4ce85552
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-22 17:30:18 +0300

    названия комп

--- downinfo.js
+++ downinfo.js
@@ -27,9 +27,56 @@
             <div class="fa fa-instagram"></div>
         </a>
     </div>
+</div>
+<div class="maincon">
+        <p>
+        <div class="compname" data-name = "ddd" id="RS">RS</div>
+        <div class="compname" id="RI">RI</div> <div> & </div>   
+        <div class="compname" id="PE">PE</div>
+        <div class="compname" id="PA">PA</div>
+        </p>
+        
+    </div>
+    <div class="maincon" style="height: 30px;">
+        <p style="font-size: 12px;"><b style="font-size: 14px;">©</b>Все права защищены 2021</p>
+    </div>`); 
 
-</div><div style = "background-color: #00ff90;display: flex;
-    justify-content: center;
-    align-items: center; 
-    height: 10vh;">ну как бы все</div>`); 
+var rs = document.getElementById("RS");
+rs.onmouseover = function () {
+    rs.innerHTML = "RebuffedStudios"
 
+}
+rs.onmouseout = function () {
+    rs.innerHTML = "RS"
+
+}
+
+var ri = document.getElementById("RI");
+ri.onmouseover = function () {
+    ri.innerHTML = "Rebuffed Inc."
+
+}
+ri.onmouseout = function () {
+    ri.innerHTML = "RI"
+
+}
+
+var pe = document.getElementById("PE");
+pe.onmouseover = function () {
+    pe.innerHTML = "Posti Entertaiment"
+
+}
+pe.onmouseout = function () {
+    pe.innerHTML = "PE"
+
+}
+
+var pa = document.getElementById("PA");
+pa.onmouseover = function () {
+    pa.innerHTML = "Posti Arts"
+
+}
+pa.onmouseout = function () {
+    pa.innerHTML = "PA"
+
+}
---
+++ js/scriptdowninf.js
@@ -0,0 +1,39 @@
+﻿var rs = document.getElementById("RS");
+rs.onmouseover = function () {
+    rs.innerHTML = "RebuffedStudios"
+
+}
+rs.onmouseout = function () {
+    rs.innerHTML = "RS"
+
+}
+
+var ri = document.getElementById("RI");
+ri.onmouseover = function () {
+    ri.innerHTML = "Rebuffed Inc."
+
+}
+ri.onmouseout = function () {
+    ri.innerHTML = "RI"
+
+}
+
+var pe = document.getElementById("PE");
+pe.onmouseover = function () {
+    pe.innerHTML = "Posti Entertaiment"
+
+}
+pe.onmouseout = function () {
+    pe.innerHTML = "PE"
+
+}
+
+var pa = document.getElementById("PA");
+pa.onmouseover = function () {
+    pa.innerHTML = "Posti Arts"
+
+}
+pa.onmouseout = function () {
+    pa.innerHTML = "PA"
+
+}
