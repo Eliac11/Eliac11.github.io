89d39dbe5926f5a70746aa6b3f16473dfd3c9a10
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-28 14:49:57 +0300

    новая менюшка

--- mainmenu.js
+++ mainmenu.js
@@ -1,32 +1,48 @@
-﻿mmenu.insertAdjacentHTML("afterend",`<div class="container--menu">
-        <div class="social--menu">
-            <a href="index.html" class="button--menu">
-                <span class="button__line--menu button__line--top--menu"></span>
-                <span class="button__line--menu button__line--bottom--menu"></span>
-                главная
-            </a>
-        </div>
-        <div class="social--menu">
-            <a href="info.html" class="button--menu">
-                <span class="button__line--menu button__line--top--menu"></span>
-                <span class="button__line--menu button__line--bottom--menu"></span>
-                инфо
-            </a>
-        </div>
-        <div class="social--menu">
-            <a href="forum.html" class="button--menu">
-                <span class="button__line--menu button__line--top--menu"></span>
-                <span class="button__line--menu button__line--bottom--menu"></span>
-                форум
-            </a>
-        </div>
-        <div class="social--menu">
-            <a href="over1.html" class="button--menu">
-                <span class="button__line--menu button__line--top--menu"></span>
-                <span class="button__line--menu button__line--bottom--menu"></span>
-                другое
-            </a>
-        </div>
+﻿//mmenu.insertAdjacentHTML("afterend",`<div class="container--menu">
+//        <div class="social--menu">
+//            <a href="index.html" class="button--menu">
+//                <span class="button__line--menu button__line--top--menu"></span>
+//                <span class="button__line--menu button__line--bottom--menu"></span>
+//                главная
+//            </a>
+//        </div>
+//        <div class="social--menu">
+//            <a href="info.html" class="button--menu">
+//                <span class="button__line--menu button__line--top--menu"></span>
+//                <span class="button__line--menu button__line--bottom--menu"></span>
+//                инфо
+//            </a>
+//        </div>
+//        <div class="social--menu">
+//            <a href="forum.html" class="button--menu">
+//                <span class="button__line--menu button__line--top--menu"></span>
+//                <span class="button__line--menu button__line--bottom--menu"></span>
+//                форум
+//            </a>
+//        </div>
+//        <div class="social--menu">
+//            <a href="over1.html" class="button--menu">
+//                <span class="button__line--menu button__line--top--menu"></span>
+//                <span class="button__line--menu button__line--bottom--menu"></span>
+//                другое
+//            </a>
+//        </div>
+//
+//    </div>`);
 
+mmenu.insertAdjacentHTML("afterend", `<div class="conmenu">
+        <nav class="dws--menu">
+            <ul>
+                <li><a href="index.html">главная</a></li>
+                <li><a href="info.html">инфо</a></li>
+                <li><a href="forum.html">фоурм</a></li>
+                <li><a>другое</a>
+                    <ul>
+                        <li><a href="over1.html">шарики</a></li>
+                        <li><a href="#">еще продукты</a></li>
+                        <li><a href="#">тест на айкю</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     </div>`);
-
