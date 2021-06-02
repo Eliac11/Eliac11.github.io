e8fe2a5184ee054a1c822ec419e063d4cebe9b19
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-06-02 21:35:33 +0300

    тссс секрет

--- upinfo.js
+++ upinfo.js
@@ -1,4 +1,4 @@
-﻿upnf.insertAdjacentHTML("afterend",`<div style="color: #b6ff00">Совершенно новый прорывной хоррор от молодой инди студии</div>
+﻿upnf.insertAdjacentHTML("afterend",`<div style="color: #b6ff00; font-size: 100%">Совершенно новый прорывной хоррор от молодой инди студии</div>
 
     <h1 style=" color: #b6ff00; display: flex;justify-content: center; align-items: center; font-size: 40px"><b id="gamename">RAFN 2</b></h1>`);
 
